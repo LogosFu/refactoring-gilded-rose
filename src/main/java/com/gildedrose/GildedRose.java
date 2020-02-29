7d0ab6ed6166f91233215259dfcae9a155ea7460
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-02-29 20:20:22 +0800

    Mysterious Name

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,19 +1,23 @@
 package com.gildedrose;
 
 class GildedRose {
+
     Item[] items;
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String TAFKL_80_ETC = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
 
     public GildedRose(Item[] items) {
         this.items = items;
     }
 
-    public void update_quality() {
+    public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             final Item item = items[i];
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!item.name.equals(AGED_BRIE)
+                    && !item.name.equals(TAFKL_80_ETC)) {
                 if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!item.name.equals(SULFURAS)) {
                         item.tallyDownQuality();
                     }
                 }
@@ -21,7 +25,7 @@ class GildedRose {
                 if (item.quality < 50) {
                     item.plusOneQuality();
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.name.equals(TAFKL_80_ETC)) {
                         if (item.sell_in < 11) {
                             if (item.quality < 50) {
                                 item.plusOneQuality();
@@ -37,15 +41,15 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!item.name.equals(SULFURAS)) {
                 item.sell_in = item.sell_in - 1;
             }
 
             if (item.sell_in < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!item.name.equals(AGED_BRIE)) {
+                    if (!item.name.equals(TAFKL_80_ETC)) {
                         if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!item.name.equals(SULFURAS)) {
                                 item.tallyDownQuality();
                             }
                         }
