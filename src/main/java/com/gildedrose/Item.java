bda92e8e4d91e567c45c5617a406491525487b31
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-03-03 21:24:47 +0800

    clean and rename

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,67 @@ public class Item {
     public String toString() {
         return this.name + ", " + this.sell_in + ", " + this.quality;
     }
+
+    void updateItemQuality() {
+        if (!isAgedBrie()
+            && !isBackstage()) {
+            if (quality > 0) {
+                if (!isSulfuras()) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (isBackstage()) {
+                    if (sell_in < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sell_in < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!isSulfuras()) {
+            sell_in = sell_in - 1;
+        }
+
+        if (sell_in < 0) {
+            if (!isAgedBrie()) {
+                if (!isBackstage()) {
+                    if (quality > 0) {
+                        if (!isSulfuras()) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = 0;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+    }
+
+    private boolean isSulfuras() {
+        return name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    private boolean isBackstage() {
+        return name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    private boolean isAgedBrie() {
+        return name.equals("Aged Brie");
+    }
 }
